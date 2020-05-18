6a3fec9f4a56d55f30a1ce72bbaddba311427fdf
Nicole Zheng <zznicole@hotmail.com>
2020-05-18 23:42:38 +0200

    parse and get data, then add to browser

--- src/script.js
+++ src/script.js
@@ -3,23 +3,15 @@ let bucketLists  = [];
 $('#addButton').on('click',function(){
  
   let bucketItem = $('#addList').val();
+  
   $('#theList').append(`<li>${bucketItem}</li>`);
   
-  // for(let i = 0; i < bucketLists.length; i++) {
     bucketLists.push(bucketItem);
-    // console.log(bucketLists);
-   
- 
-// alert(listArray);
-//  function addItem (item) {
-//    for(let i = 0; i < listArray.length; i++) {
-//      localStorage.setItem('localArray',JSON.stringify('localArray'));
-
-//   }
-
-console.log(typeof(bucketLists));
-//  }
+  
  localStorage.setItem("bucketlists", JSON.stringify(bucketLists));
 });
 
-// let bucketLists = JSON.parse(localStorage.getItem)("bucketLists");
+
+let bucketlists = JSON.parse(localStorage.getItem("bucketlists"));
+
+$('#theList').append(`<li> ${bucketlists} </li>`);
