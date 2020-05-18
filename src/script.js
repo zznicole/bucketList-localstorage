98d3aca7ccf2b73c30a3eae0a24c4773c6eb0c2c
Nicole Zheng <zznicole@hotmail.com>
2020-05-18 21:50:36 +0200

    set data in localStorage as objects

--- src/script.js
+++ src/script.js
@@ -1,8 +1,25 @@
+let bucketLists  = [];
+
 $('#addButton').on('click',function(){
  
-  let task = $('#addList').val();
-  let  
-  $('#theList').append(`<li>${task}</li>`);
-  localStorage.setItem(``,`${task}`);
-});
+  let bucketItem = $('#addList').val();
+  $('#theList').append(`<li>${bucketItem}</li>`);
+  
+  // for(let i = 0; i < bucketLists.length; i++) {
+    bucketLists.push(bucketItem);
+    // console.log(bucketLists);
+   
  
+// alert(listArray);
+//  function addItem (item) {
+//    for(let i = 0; i < listArray.length; i++) {
+//      localStorage.setItem('localArray',JSON.stringify('localArray'));
+
+//   }
+
+console.log(typeof(bucketLists));
+//  }
+ localStorage.setItem("bucketlists", JSON.stringify(bucketLists));
+});
+
+// let bucketLists = JSON.parse(localStorage.getItem)("bucketLists");
